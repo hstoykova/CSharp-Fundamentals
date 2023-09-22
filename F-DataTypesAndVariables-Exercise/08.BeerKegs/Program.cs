a44434e88ef0d4a46a7b1d441c8d4a73295a2dc6
hstoykova <hristina.pencheva@gmail.com>
2023-09-22 22:33:39 +0300

    08BeerKegs solved

---
+++ F-DataTypesAndVariables-Exercise/08.BeerKegs/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine());
+double volume = 0;
+string largest = null;
+
+for (int i = 0; i < n; i++)
+{
+    string model = Console.ReadLine();
+    double radius = double.Parse(Console.ReadLine());
+    int height = int.Parse(Console.ReadLine());
+
+    double currentVolume = Math.PI * (radius * radius) * height;
+
+    if (currentVolume > volume)
+    {
+        largest = model;
+        volume = currentVolume;
+    }
+}
+Console.WriteLine(largest);
