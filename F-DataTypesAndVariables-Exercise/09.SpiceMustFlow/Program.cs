0c75caf0f9087f5caf264d73eaeb6af760328e0b
hstoykova <hristina.pencheva@gmail.com>
2023-09-23 13:36:17 +0300

    09. "Spice Must Flow" solved

---
+++ F-DataTypesAndVariables-Exercise/09.SpiceMustFlow/Program.cs
@@ -0,0 +1,22 @@
+﻿int dailyYield = int.Parse(Console.ReadLine());
+int workersConsume = 26;
+int collectedYield = 0;
+int days = 0;
+
+while (dailyYield >= 100)
+{
+    collectedYield += dailyYield;
+    collectedYield -= workersConsume;
+    dailyYield -= 10;
+    days++;
+}
+
+collectedYield -= workersConsume;
+
+if (collectedYield < 0)
+{
+    collectedYield = 0;
+}
+
+Console.WriteLine(days);
+Console.WriteLine(collectedYield);
