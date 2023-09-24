c420878c5137aa7dc0a3a8b5c61527da99cb25d6
hstoykova <hristina.pencheva@gmail.com>
2023-09-24 18:20:34 +0300

    10. Poke Mon and 11. Snowballs solved

---
+++ F-DataTypesAndVariables-Exercise/10.PokeMon/Program.cs
@@ -0,0 +1,19 @@
+﻿int NpokePower = int.Parse(Console.ReadLine());
+int MdistansBetweenTargets = int.Parse(Console.ReadLine());
+int YexhaustionFactor = int.Parse(Console.ReadLine());
+
+int NMSubstraction = NpokePower;
+int numberOfTargets = 0;
+
+while (NMSubstraction >=  MdistansBetweenTargets)
+{
+    NMSubstraction -= MdistansBetweenTargets;
+    numberOfTargets++;
+
+    if (NMSubstraction == NpokePower / 2 && YexhaustionFactor != 0)
+    {
+        NMSubstraction = NMSubstraction / YexhaustionFactor;
+    }
+}
+Console.WriteLine(NMSubstraction);
+Console.WriteLine(numberOfTargets);
---
+++ F-DataTypesAndVariables-Exercise/11.Snowballs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+int numberOfSnowballs = int.Parse(Console.ReadLine());
+BigInteger snowballValue = 0;
+BigInteger bestSnowballValue = snowballValue;
+
+int bestSnowballSnow = 0;
+int bestSnowballTime = 0;
+int bestSnowballQuality = 0;
+
+for (int i = 0; i < numberOfSnowballs; i++)
+{
+    int snowballSnow = int.Parse(Console.ReadLine());
+    int snowballTime = int.Parse(Console.ReadLine());
+    int snowballQuality = int.Parse(Console.ReadLine());
+    snowballValue = BigInteger.Pow(snowballSnow / snowballTime, snowballQuality);
+
+    if (snowballValue > bestSnowballValue)
+    {
+        bestSnowballValue = snowballValue;
+        bestSnowballSnow = snowballSnow;
+        bestSnowballTime = snowballTime;
+        bestSnowballQuality = snowballQuality;
+    }
+}
+Console.WriteLine($"{bestSnowballSnow} : {bestSnowballTime} = {bestSnowballValue} ({bestSnowballQuality})");
