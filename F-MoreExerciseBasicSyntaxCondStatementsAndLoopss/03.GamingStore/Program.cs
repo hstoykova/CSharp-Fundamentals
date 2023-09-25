7622557610062b2c025f1429f30cf5500dc1c651
hstoykova <hristina.pencheva@gmail.com>
2023-09-25 10:40:33 +0300

    "03. Gaming Store" solved

---
+++ F-MoreExerciseBasicSyntaxCondStatementsAndLoopss/03.GamingStore/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Reflection;
+
+double currentBalance = double.Parse(Console.ReadLine());
+double newBalance = currentBalance;
+double totalSpent = 0;
+string game = Console.ReadLine();
+
+while (game != "Game Time")
+{
+
+    double gamePrice = 0;
+
+    if (game == "OutFall 4")
+    {
+        gamePrice = 39.99;
+        if (newBalance < gamePrice)
+        {
+            Console.WriteLine("Too Expensive");
+        }
+        else
+        {
+            totalSpent += gamePrice;
+            newBalance -= gamePrice;
+            Console.WriteLine($"Bought {game}");
+        }
+        
+    }
+
+    else if (game == "CS: OG")
+    {
+        gamePrice = 15.99;
+        if (newBalance < gamePrice)
+        {
+            Console.WriteLine("Too Expensive");
+        }
+        else
+        {
+            totalSpent += gamePrice;
+            newBalance -= gamePrice;
+            Console.WriteLine($"Bought {game}");
+        }
+    }
+
+    else if (game == "Zplinter Zell")
+    {
+        gamePrice = 19.99;
+        if (newBalance < gamePrice)
+        {
+            Console.WriteLine("Too Expensive");
+        }
+        else
+        {
+            totalSpent += gamePrice;
+            newBalance -= gamePrice;
+            Console.WriteLine($"Bought {game}");
+        }
+    }
+    
+    else if (game == "Honored 2")
+    {
+        gamePrice = 59.99;
+        if (newBalance < gamePrice)
+        {
+            Console.WriteLine("Too Expensive");
+        }
+        else
+        {
+            totalSpent += gamePrice;
+            newBalance -= gamePrice;
+            Console.WriteLine($"Bought {game}");
+        }
+    }
+
+    else if (game == "RoverWatch")
+    {
+        gamePrice = 29.99;
+        if (newBalance < gamePrice)
+        {
+            Console.WriteLine("Too Expensive");
+        }
+        else
+        {
+            totalSpent += gamePrice;
+            newBalance -= gamePrice;
+            Console.WriteLine($"Bought {game}");
+        }
+    }
+    else if (game == "RoverWatch Origins Edition")
+    {
+        gamePrice = 39.99;
+        if (newBalance < gamePrice)
+        {
+            Console.WriteLine("Too Expensive");
+        }
+        else
+        {
+            totalSpent += gamePrice;
+            newBalance -= gamePrice;
+            Console.WriteLine($"Bought {game}");
+        }
+    }
+    else
+        {
+            Console.WriteLine("Not Found");
+        }
+
+    if (newBalance == 0)
+        {
+            Console.WriteLine("Out of money!");
+            break;
+        }
+     game = Console.ReadLine();
+}
+
+Console.WriteLine($"Total spent: ${totalSpent:F2}. Remaining: ${currentBalance - totalSpent:F2}");
