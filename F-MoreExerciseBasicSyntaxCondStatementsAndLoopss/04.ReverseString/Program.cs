56d3aa5a366362182f159c95dc76d54bed29c543
hstoykova <hristina.pencheva@gmail.com>
2023-09-25 11:06:38 +0300

    "04. Reverse String" solved

---
+++ F-MoreExerciseBasicSyntaxCondStatementsAndLoopss/04.ReverseString/Program.cs
@@ -0,0 +1,8 @@
+﻿string originalString = Console.ReadLine();
+
+string reverseString = string.Empty;
+for (int i = originalString.Length - 1; i >= 0; i--)
+{
+    reverseString += originalString[i];
+}
+Console.WriteLine(reverseString);
