7664cdfbef2a068af8108d1281cb17811222fe6e
hstoykova <hristina.pencheva@gmail.com>
2023-09-25 09:03:33 +0300

    New folder created

---
+++ F-MoreExercise-BasicSyntaxConditionalStatementsAndLoopss/01.SortNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
+
+//int[] n = new int[3];
+
+//n[0] = firstNumber;
+//n[1] = secondNumber;
+//n[2] = thirdNumber;
+
+//int[] sorted = n.OrderByDescending(x => x).ToArray();
+
+
+List<int> numbers = new List<int> { firstNumber, secondNumber, thirdNumber };
+List<int> sorted = numbers.OrderByDescending(n => n).ToList();
+
+
+for (int i = 0; i < sorted.Count; i++)
+{
+    Console.WriteLine(sorted[i]);
+}
