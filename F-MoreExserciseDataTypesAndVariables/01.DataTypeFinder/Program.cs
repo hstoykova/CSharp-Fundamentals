620ccadab9c64b34ca1e9359f240c1c97e038821
hstoykova <hristina.pencheva@gmail.com>
2023-09-28 21:30:12 +0300

    "01. Data Type Finder" solved

---
+++ F-MoreExserciseDataTypesAndVariables/01.DataTypeFinder/Program.cs
@@ -0,0 +1,28 @@
+﻿string input = Console.ReadLine();
+string dataType = null;
+
+while (input != "END")
+{
+    if (Int32.TryParse(input, out _))
+    {
+        dataType = "integer";
+    }
+    else if (float.TryParse(input, out _))
+    {
+        dataType = "floating point";
+    }
+    else if (char.TryParse(input, out _))
+    {
+        dataType = "character";
+    }
+    else if (bool.TryParse(input, out _))
+    {
+        dataType = "boolean";
+    }
+    else
+    {
+        dataType = "string"; 
+    }
+    Console.WriteLine($"{input} is {dataType} type");
+    input = Console.ReadLine();
+}
