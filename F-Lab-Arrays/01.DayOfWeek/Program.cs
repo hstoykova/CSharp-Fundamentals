c372e309d1bc885a710f1cf5aae5bcbac9045dc1
hstoykova <hristina.pencheva@gmail.com>
2023-09-29 16:19:16 +0300

    "02. Print Numbers in Reverse Order" solved

---
+++ F-Lab-Arrays/01.DayOfWeek/Program.cs
@@ -0,0 +1,20 @@
+﻿int day = int.Parse(Console.ReadLine());
+
+string[] days = {
+"Monday",
+"Tuesday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday",
+"Sunday"
+};
+
+if (day >= 1 && day <= 7)
+{
+    Console.WriteLine(days[day - 1]);
+}
+else
+{
+    Console.WriteLine("Invalid day!");
+}
