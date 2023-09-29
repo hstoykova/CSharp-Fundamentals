64aa06dbf641b3137c172a5e2712ce05625b6fd0
hstoykova <hristina.pencheva@gmail.com>
2023-09-29 22:10:35 +0300

    '04. Reverse Array of Strings' solved

---
+++ F-Lab-Arrays/04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] elements = Console.ReadLine()
+    .Split()
+    .ToArray();
+
+for (int i = 0; i < elements.Length / 2; i++)
+{
+    string oldElement = elements[i];
+    elements[i] = elements[elements.Length - 1 - i];
+    elements[elements.Length - 1 - i] = oldElement;
+}
+Console.WriteLine(string.Join(" ", elements));
+
+
+// Another solution:
+
+//string[] elements = Console.ReadLine()
+//    .Split()
+//    .ToArray();
+
+//string[] reversed = new string[elements.Length];
+
+//for (int i = 0; i < elements.Length; i++)
+//{
+//    reversed[elements.Length - 1 - i] = elements[i];
+//}
+//Console.WriteLine(string.Join(" ", reversed));
