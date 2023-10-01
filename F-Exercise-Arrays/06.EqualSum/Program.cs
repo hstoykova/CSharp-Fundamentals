720fa98a9c3fc192511e0af5865d22c65aaf02c0
hstoykova <hristina.pencheva@gmail.com>
2023-10-01 11:32:31 +0300

    "06. Equal Sum" solved

---
+++ F-Exercise-Arrays/06.EqualSum/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] numbers = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToArray();
+
+int index = -1;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    index = -1;
+    int leftSum = 0;
+    int rightSum = 0;
+
+    for (int j = i - 1; j >= 0; j--)
+    {
+        leftSum += numbers[j];
+    }
+
+    for (int k = i + 1; k < numbers.Length; k++)
+    {
+        rightSum += numbers[k];
+    }
+
+    if (leftSum == rightSum)
+    {
+        index = i;
+        break;
+    }
+}
+
+if (index != -1)
+{
+    Console.WriteLine(index);
+}
+else
+{
+    Console.WriteLine("no");
+}
