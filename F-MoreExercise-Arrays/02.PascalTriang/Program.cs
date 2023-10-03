8f3751bbfced5dbf05e17446a9cf926277247d97
hstoykova <hristina.pencheva@gmail.com>
2023-10-03 09:00:31 +0300

    "Demo" file created

--- F-MoreExercise-Arrays/02.PascalTriang/Program.cs
+++ F-MoreExercise-Arrays/02.PascalTriang/Program.cs
@@ -1,10 +1,10 @@
 ﻿int n = int.Parse(Console.ReadLine());
 
-for (int i = 0; i < n; i++)
+for (int i = 0; i < n; i++) // row
 {
     int number = 1;
 
-    for (int j = 0; j <= i; j++)
+    for (int j = 0; j <= i; j++) // column
     {
         Console.Write($"{number} ");
         number = number * (i - j) / (j + 1);
