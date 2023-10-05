6a267370333488f176a79c017f516f9c4db45500
hstoykova <hristina.pencheva@gmail.com>
2023-10-05 14:37:24 +0300

    "04. Refactoring: Prime Checker" solved

---
+++ F-MoreExserciseDataTypesAndVariables/04.RefactoringPrimeChecker/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+for (int i = 2; i <= number; i++)
+{
+    bool isTrue = true;
+    
+    for (int j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            isTrue = false;
+            break;
+        }
+    }
+    Console.WriteLine($"{i} -> {isTrue.ToString().ToLower()}");
+}
