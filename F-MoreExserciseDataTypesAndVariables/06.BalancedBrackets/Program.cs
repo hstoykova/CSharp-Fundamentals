bab389128ea6bb2895f7b360a8a7a4020b1b0ccf
hstoykova <hristina.pencheva@gmail.com>
2023-10-05 15:34:35 +0300

    "06. Balanced Brackets" solved

---
+++ F-MoreExserciseDataTypesAndVariables/06.BalancedBrackets/Program.cs
@@ -0,0 +1,41 @@
+﻿int numberOfLines = int.Parse(Console.ReadLine());
+bool isOpened = false;
+bool isBalanced = true;
+
+for (int n = 0; n < numberOfLines; n++)
+{
+    string input = Console.ReadLine();
+
+    if (input == "(")
+    {
+        if (!isOpened)
+        {
+            isOpened = true;
+        }
+        else
+        {
+            isBalanced = false;
+        }
+    }
+
+    if (input == ")")
+    {
+        if (isOpened)
+        {
+            isOpened = false;
+        }
+        else
+        {
+            isBalanced = false;
+        }
+    }
+}
+
+if (isBalanced && !isOpened)
+{
+    Console.WriteLine("BALANCED");
+}
+else
+{
+    Console.WriteLine("UNBALANCED");
+}
