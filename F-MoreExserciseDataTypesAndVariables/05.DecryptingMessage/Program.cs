23f0409dd5634d4eca2adde2355f01df5622577e
hstoykova <hristina.pencheva@gmail.com>
2023-10-05 15:22:03 +0300

    "05. Decrypting Message" solved

---
+++ F-MoreExserciseDataTypesAndVariables/05.DecryptingMessage/Program.cs
@@ -0,0 +1,12 @@
+﻿int key = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+string position = null;
+
+for (int i = 0; i < n; i++)
+{
+    char letter = char.Parse(Console.ReadLine());
+    int letterToNum = (int)(letter) + key;
+    char currentLetter = (char)letterToNum;
+    position += currentLetter;
+}
+Console.WriteLine(position);
