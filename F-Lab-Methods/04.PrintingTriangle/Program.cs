452cd2256ca60c764b0dbd0b0999cec82a299469
hstoykova <hristina.pencheva@gmail.com>
2023-10-06 18:08:10 +0300

    "04. Printing Triangle" solved

---
+++ F-Lab-Methods/04.PrintingTriangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Common;
+
+int n = int.Parse(Console.ReadLine());
+
+for (int row = 1; row <= n; row++)
+{
+    PrintRow(row);
+}
+
+for (int row = n-1; row >= 0; row--)
+{
+    PrintRow(row);
+}
+
+static void PrintRow(int row)
+{
+    for (int column = 1; column <= row; column++)
+    {
+        Console.Write(column + " ");
+    }
+    Console.WriteLine();
+}
