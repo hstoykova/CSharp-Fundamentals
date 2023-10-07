d54af14510ebd2e74d3a4f0b73f3b0c2128aeda9
hstoykova <hristina.pencheva@gmail.com>
2023-10-07 09:59:15 +0300

    "06. Calculate Rectangle Area" solved

---
+++ F-Lab-Methods/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,11 @@
+﻿double width = double.Parse(Console.ReadLine());
+double height = double.Parse(Console.ReadLine());
+
+double aria = GetRectangleArea(width, height);
+
+Console.WriteLine(aria);
+
+static double GetRectangleArea(double width,double height)
+{
+    return width * height;
+}
