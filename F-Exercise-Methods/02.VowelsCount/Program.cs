3d68b6b422b88b4ad727b8d8ce6cf64b587d7a11
hstoykova <hristina.pencheva@gmail.com>
2023-10-07 23:23:31 +0300

    "02. Vowels Count" solved

---
+++ F-Exercise-Methods/02.VowelsCount/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _02.VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] digits = text
+                .ToString()
+                .ToLower()
+                .ToCharArray()
+                .Select(char.ToString)
+                .ToArray();
+
+            int vowelsCount = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (digits[i] == "a" || digits[i] == "e" || digits[i] == "i" ||
+                    digits[i] == "o" || digits[i] == "u")
+                {
+                    vowelsCount++;
+                }
+            }
+            Console.WriteLine(vowelsCount);
+        }
+    }
+}
