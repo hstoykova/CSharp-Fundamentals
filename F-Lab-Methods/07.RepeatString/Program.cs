fd1098f33c223364265ab0eb9b7a439b435c3ad2
hstoykova <hristina.pencheva@gmail.com>
2023-10-07 10:11:57 +0300

    "07. Repeat String" solved

---
+++ F-Lab-Methods/07.RepeatString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Channels;
+
+string initialStr = Console.ReadLine();
+int repeatedTimes = int.Parse(Console.ReadLine());
+
+NewCreatedString(initialStr, repeatedTimes);
+
+static string NewCreatedString(string initialStr, int repeatedTimes)
+{
+    string result = "";
+    for (int i = 0; i < repeatedTimes; i++)
+    {
+        result += initialStr;
+    }
+    Console.WriteLine(result);
+    return result;
+}
