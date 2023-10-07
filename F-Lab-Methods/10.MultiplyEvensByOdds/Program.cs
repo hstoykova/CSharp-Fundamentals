e9e0d8da1d9649fde96aa2f5e00e19546712769b
hstoykova <hristina.pencheva@gmail.com>
2023-10-07 15:26:13 +0300

    "10. Multiply Evens by Odds" solved

--- F-Lab-Methods/10.MultiplyEvensByOdds/Program.cs
+++ F-Lab-Methods/10.MultiplyEvensByOdds/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+            int[] digits = number
+                .ToString()
+                .ToCharArray()
+                .Select(char.ToString)
+                .Select(int.Parse)
+                .ToArray();
+
+            int sumEven = 0;
+            int sumOdd = 0;
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i] % 2 == 0)
+                {
+                    sumEven += digits[i];
+                }
+                else
+                {
+                    sumOdd += digits[i];
+                }
+
+            }
+
+            Console.WriteLine($"{sumEven * sumOdd}");
         }
     }
 }
