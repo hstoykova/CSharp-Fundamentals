52b58719f606c6e4bf28b8709745ba184c64393d
hstoykova <hristina.pencheva@gmail.com>
2023-10-07 10:22:42 +0300

    "08. Math Power" solved

---
+++ F-Lab-Methods/08.MathPower/Program.cs
@@ -0,0 +1,10 @@
+﻿double number = double.Parse(Console.ReadLine());
+double power = double.Parse(Console.ReadLine());
+
+double result = ReturnPow(number, power);
+Console.WriteLine(result);
+
+double ReturnPow(double number, double power)
+{
+    return Math.Pow(number, power);
+}
