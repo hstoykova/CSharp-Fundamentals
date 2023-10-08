8d68c712f40a48a36a46ef803b7c046be9f90908
hstoykova <hristina.pencheva@gmail.com>
2023-10-08 15:05:28 +0300

    "08. Factorial Division" solved

---
+++ F-Exercise-Methods/08.FactorialDivision/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _08.FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long firstNum = long.Parse(Console.ReadLine());
+            long secondNum = long.Parse(Console.ReadLine());
+
+            double firstFact = Factorial(firstNum);
+            double secondFact = Factorial(secondNum);
+
+            double result = firstFact/secondFact;
+
+            Console.WriteLine($"{result:f2}");
+        }
+        static double Factorial(long n)
+        {
+            double fact = 1;
+
+            for (long i = n; i > 0; i--)
+            {
+                fact *= i;
+            }
+
+            return fact;
+        }
+    }
+}
