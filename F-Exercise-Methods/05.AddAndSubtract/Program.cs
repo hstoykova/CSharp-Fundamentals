bd683a767e98dda3f1ac952f9b58fa24975032e1
hstoykova <hristina.pencheva@gmail.com>
2023-10-08 14:41:26 +0300

    "06. Middle Characters" solved

---
+++ F-Exercise-Methods/05.AddAndSubtract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _05.AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int sum = Add(firstNum, secondNum);
+            Console.WriteLine(Subtract(sum, thirdNum));
+        }
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Subtract(int b, int c)
+        {
+            return (b - c);
+        }
+    }
+}
---
+++ F-Exercise-Methods/06.MiddleCharacters/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _06.MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string someText = Console.ReadLine();
+
+            Console.WriteLine(GetMiddleChars(someText));
+        }
+
+        static string GetMiddleChars(string text)
+        {
+            if (text.Length % 2 != 0)
+            {
+                return text[text.Length / 2].ToString();
+            }
+            else
+            {
+                return $"{text[text.Length / 2 - 1]}{text[text.Length / 2]}";
+            }
+        }
+    }
+}
