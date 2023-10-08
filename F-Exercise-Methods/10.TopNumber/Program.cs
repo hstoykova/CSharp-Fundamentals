cfedfa292dea16a17cf74f27ddc5c44ee2a474e2
hstoykova <hristina.pencheva@gmail.com>
2023-10-08 15:40:50 +0300

    "10. Top Number" solved

---
+++ F-Exercise-Methods/10.TopNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _10.TopNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (IsTopNumber(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static bool IsTopNumber(int n)
+        {
+            string numberToString = n.ToString();
+            int sum = 0;
+            int oddCount = 0;
+
+            for (int i = 0; i < numberToString.Length; i++)
+            {
+                int currentNumber = int.Parse(numberToString[i].ToString());
+                sum += currentNumber;
+
+                if (currentNumber % 2 != 0)
+                {
+                    oddCount++;
+                }
+            }
+
+            return sum % 8 == 0 && oddCount >= 1;
+        }
+
+    }
+}
