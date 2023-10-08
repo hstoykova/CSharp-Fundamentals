2b87861a6612f0f2ede1552fb75715182f05bdf6
hstoykova <hristina.pencheva@gmail.com>
2023-10-08 15:21:39 +0300

    "09. Palindrome Integers" solved

---
+++ F-Exercise-Methods/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _09.PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numStr;
+
+            while ((numStr = Console.ReadLine()) != "END")
+            {
+                Console.WriteLine(IsPalindrome(numStr).ToString().ToLower());
+            }
+        }
+
+        static bool IsPalindrome(string input)
+        {
+            for (int i = 0; i < input.Length/2; i++)
+            {
+                if (input[i] != input[input.Length-1-i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
