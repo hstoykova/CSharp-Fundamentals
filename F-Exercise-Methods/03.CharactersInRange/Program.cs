ad41251715be2113cced2211af651c5ed1aa0179
hstoykova <hristina.pencheva@gmail.com>
2023-10-08 10:21:25 +0300

    "03. Characters in Range" solved

---
+++ F-Exercise-Methods/03.CharactersInRange/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03.CharactersInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            int startCharacter = Math.Min(firstChar, secondChar);
+            int endCharacter = Math.Max(firstChar, secondChar);
+
+            for (int i = ++startCharacter; i < endCharacter; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
