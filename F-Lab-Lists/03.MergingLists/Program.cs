84e861398fd386a8c1e186a4b86299e75ccb9adb
hstoykova <hristina.pencheva@gmail.com>
2023-10-12 21:39:10 +0300

    "03. Merging Lists" solved

--- F-Lab-Lists/03.MergingLists/Program.cs
+++ F-Lab-Lists/03.MergingLists/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace _03.MergingLists
+﻿using System.Collections.Generic;
+using System.Threading.Channels;
+
+namespace _03.MergingLists
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> firstLine = ReadIntList();
+            List<int> secondLine = ReadIntList();
+            List<int> finalLine = new List<int>();
+
+            int arrayLen = Math.Max(firstLine.Count, secondLine.Count);
+
+            for (int i = 0; i < arrayLen; i++)
+            {
+                if (i < firstLine.Count)
+                {
+                    finalLine.Add(firstLine[i]);
+                }
+                if (i < secondLine.Count)
+                {
+                    finalLine.Add(secondLine[i]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", finalLine));
+
+        }
+
+        static List<int> ReadIntList()
+        {
+            return Console.ReadLine()
+           .Split()
+           .Select(int.Parse)
+           .ToList();
         }
     }
 }
