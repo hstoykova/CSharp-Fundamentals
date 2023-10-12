8dc4b01d99a5ffaa4c5f8be1a7eb1d3830e26ec0
hstoykova <hristina.pencheva@gmail.com>
2023-10-12 16:44:29 +0300

    "02. Gauss' Trick" solved

--- F-Lab-Lists/02.GaussTrick/Program.cs
+++ F-Lab-Lists/02.GaussTrick/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Numerics;
 
 namespace _02.GaussTrick
 {
@@ -6,7 +7,27 @@ namespace _02.GaussTrick
     {
         static void Main(string[] args)
         {
-            
+            List<int> numbers = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+                int first = numbers[i];
+                int last = numbers[numbers.Count - i - 1];
+
+                result.Add(first + last);
+            }
+
+            if (numbers.Count % 2 == 1)
+            {
+                int middle = numbers.Count / 2;
+                result.Add(numbers[middle]);
+            }
+            Console.WriteLine(string.Join(" ", result));
         }
     }
 }
