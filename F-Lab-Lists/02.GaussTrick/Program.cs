df7b21d7a4c76164a2e74d4f4f43d288985e51fa
hstoykova <hristina.pencheva@gmail.com>
2023-10-12 14:57:08 +0300

    "01. Sum Adjacent Equal Numbers" solved

---
+++ F-Lab-Lists/02.GaussTrick/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace _02.GaussTrick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
