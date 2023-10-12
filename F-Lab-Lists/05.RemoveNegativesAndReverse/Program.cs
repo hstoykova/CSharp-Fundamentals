ea7ce891ae433d72157e8a6e93f552e259c5a994
hstoykova <hristina.pencheva@gmail.com>
2023-10-12 22:13:20 +0300

    "05. Remove Negatives and Reverse" solved

---
+++ F-Lab-Lists/05.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace _05.RemoveNegativesAndReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = ReadIntList();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+                
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+
+            numbers.Reverse();
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+
+        static List<int> ReadIntList()
+        {
+            return Console.ReadLine()
+           .Split()
+           .Select(int.Parse)
+           .ToList();
+        }
+    }
+}
