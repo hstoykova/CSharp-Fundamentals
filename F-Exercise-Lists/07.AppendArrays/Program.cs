75cc7fd0b090e10b34ae82d6b4af1bdcc58aeb02
hstoykova <hristina.pencheva@gmail.com>
2023-10-15 15:25:00 +0300

    "07. Append Arrays" solved

---
+++ F-Exercise-Lists/07.AppendArrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _07.AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+                .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<string> appended = new List<string>();
+
+            for (int i = input.Count - 1; i >= 0; i--)
+            {
+                string[] range = input[i].Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                appended.AddRange(range);
+            }
+
+            Console.WriteLine(string.Join(" ", appended));
+        }
+    }
+}
