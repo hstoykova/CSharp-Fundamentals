10bcbaba4d0c051d6d58694b61dcb7896fcb3935
hstoykova <hristina.pencheva@gmail.com>
2023-10-15 21:40:42 +0300

    "07. List Manipulation Advanced" solved

--- F-Lab-Lists/07.ListManipulationAdvanced/Program.cs
+++ F-Lab-Lists/07.ListManipulationAdvanced/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel;
 
-namespace _07.ListManipulationAdvanced
+namespace _06.ListManipulationBasics
 {
     internal class Program
     {
@@ -9,16 +8,42 @@ namespace _07.ListManipulationAdvanced
         {
             List<int> numbers = ReadIntList();
             string command;
+            bool isChanged = false;
 
-            while ((command=Console.ReadLine()) != "end")
+            while ((command = Console.ReadLine()) != "end")
             {
                 string[] newCommand = command.Split().ToArray();
+                string firstCommand = newCommand[0];
 
-                //string firstCommand = newCommand[0];
-
-                if (newCommand[0] == "Contains")
+                if (firstCommand == "Add")
                 {
-                    if (newCommand[1] == numbers.ToString())
+                    int number = int.Parse(newCommand[1]);
+                    numbers.Add(number);
+                    isChanged = true;
+                }
+                else if (firstCommand == "Insert")
+                {
+                    int number = int.Parse(newCommand[1]);
+                    int index = int.Parse(newCommand[2]);
+                    numbers.Insert(index, number);
+                    isChanged = true;
+                }
+                else if (firstCommand == "Remove")
+                {
+                    int number = int.Parse(newCommand[1]);
+                    numbers.Remove(number);
+                    isChanged = true;
+                }
+                else if ((firstCommand == "RemoveAt")) //RemoveAt
+                {
+                    int number = int.Parse(newCommand[1]);
+                    numbers.RemoveAt(number);
+                    isChanged = true;
+                }
+                else if (newCommand[0] == "Contains")
+                {
+                    int number = int.Parse(newCommand[1]);
+                    if (numbers.Contains(number))
                     {
                         Console.WriteLine("Yes");
                     }
@@ -27,22 +52,45 @@ namespace _07.ListManipulationAdvanced
                         Console.WriteLine("No such number");
                     }
                 }
-
-                    for (int i = 0; i < numbers.Count; i++)
+                else if (newCommand[0] == "PrintEven")
+                {
+                    Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 == 0)));
+                }
+                else if (newCommand[0] == "PrintOdd")
+                {
+                    Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 != 0)));
+                }
+                else if (newCommand[0] == "GetSum")
+                {
+                    Console.WriteLine(string.Join(" ", numbers.Sum()));
+                }
+                else if (newCommand[0] == "Filter")
+                {
+                    string condition = newCommand[1];
+                    int number = int.Parse(newCommand[2]);
+                    switch (condition)
                     {
-                        if (newCommand[0] == "PrintEven" && numbers[i] % 2 == 0)
-                        {
-                            Console.WriteLine(string.Join(" ", numbers));
-                        }
-                    if (newCommand[0] == "PrintOdd" && numbers[i] % 2 == 1)
-                    {
-                        Console.WriteLine(string.Join(" ", numbers));
+                        case "<":
+                            Console.WriteLine(string.Join(" ", numbers.Where(x => x < number)));
+                            break;
+                        case ">":
+                            Console.WriteLine(string.Join(" ", numbers.Where(x => x > number)));
+                            break;
+                        case ">=":
+                            Console.WriteLine(string.Join(" ", numbers.Where(x => x >= number)));
+                            break;
+                        case "<=":
+                            Console.WriteLine(string.Join(" ", numbers.Where(x => x <= number)));
+                            break;
                     }
                 }
-                
             }
-
+            if (isChanged)
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
         }
+
         static List<int> ReadIntList()
         {
             return Console.ReadLine()
@@ -50,6 +98,5 @@ namespace _07.ListManipulationAdvanced
            .Select(int.Parse)
            .ToList();
         }
-
     }
 }
--- F-Lab-Lists/Demo/Program.cs
+++ F-Lab-Lists/Demo/Program.cs
@@ -8,36 +8,42 @@ namespace _06.ListManipulationBasics
         {
             List<int> numbers = ReadIntList();
             string command;
+            bool isChanged = false;
 
             while ((command = Console.ReadLine()) != "end")
             {
-
                 string[] newCommand = command.Split().ToArray();
                 string firstCommand = newCommand[0];
 
-                int number = int.Parse(newCommand[1]);
-
                 if (firstCommand == "Add")
                 {
+                    int number = int.Parse(newCommand[1]);
                     numbers.Add(number);
+                    isChanged = true;
                 }
                 else if (firstCommand == "Insert")
                 {
+                    int number = int.Parse(newCommand[1]);
                     int index = int.Parse(newCommand[2]);
                     numbers.Insert(index, number);
+                    isChanged = true;
                 }
                 else if (firstCommand == "Remove")
                 {
+                    int number = int.Parse(newCommand[1]);
                     numbers.Remove(number);
+                    isChanged = true;
                 }
                 else if ((firstCommand == "RemoveAt")) //RemoveAt
                 {
+                    int number = int.Parse(newCommand[1]);
                     numbers.RemoveAt(number);
+                    isChanged = true;
                 }
-
-                else if(newCommand[0] == "Contains")
+                else if (newCommand[0] == "Contains")
                 {
-                    if (newCommand[1] == numbers.ToString())
+                    int number = int.Parse(newCommand[1]);
+                    if (numbers.Contains(number))
                     {
                         Console.WriteLine("Yes");
                     }
@@ -50,10 +56,39 @@ namespace _06.ListManipulationBasics
                 {
                     Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 == 0)));
                 }
-
+                else if (newCommand[0] == "PrintOdd")
+                {
+                    Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 != 0)));
+                }
+                else if (newCommand[0] == "GetSum")
+                {
+                    Console.WriteLine(string.Join(" ", numbers.Sum()));
+                }
+                else if (newCommand[0] == "Filter")
+                {
+                    string condition = newCommand[1];
+                    int number = int.Parse(newCommand[2]);
+                    switch (condition)
+                    {
+                        case "<":
+                            Console.WriteLine(string.Join(" ", numbers.Where(x => x < number)));
+                            break;
+                        case ">":
+                            Console.WriteLine(string.Join(" ", numbers.Where(x => x > number)));
+                            break;
+                        case ">=":
+                            Console.WriteLine(string.Join(" ", numbers.Where(x => x >= number)));
+                            break;
+                        case "<=":
+                            Console.WriteLine(string.Join(" ", numbers.Where(x => x <= number)));
+                            break;
+                    }
+                }
             }
-            Console.WriteLine(string.Join(" ", numbers));
-
+            if (isChanged)
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
         }
 
         static List<int> ReadIntList()
