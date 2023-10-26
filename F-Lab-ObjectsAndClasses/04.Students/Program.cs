b5abf7329063261c34c49d367a75919dc798e6a1
hstoykova <hristina.pencheva@gmail.com>
2023-10-26 22:07:24 +0300

    "04. Students" solved

---
+++ F-Lab-ObjectsAndClasses/04.Students/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+
+John Smith 15 Sofia
+Peter Ivanov 14 Plovdiv
+Linda Bridge 16 Sofia
+Simon Stone 12 Varna
+end
+Sofia
+
+Anthony Taylor 15 Chicago
+David Anderson 16 Washington
+Jack Lewis 14 Chicago
+David Lee 14 Chicago
+end
+Chicago
+
+ */
+
+namespace _04.Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentInfo;
+            List<Student> students = new List<Student>();
+
+            while ((studentInfo = Console.ReadLine()) != "end")
+            {
+                string[] tokens = studentInfo.Split();
+
+                string firstName = tokens[0];
+                string lastName = tokens[1];
+                int age = int.Parse(tokens[2]);
+                string homeTown = tokens[3];
+
+                Student student = new Student()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Age = age,
+                    HomeTown = homeTown
+                };
+                students.Add(student);
+            }
+
+            string cityName = Console.ReadLine();
+
+            foreach (Student student in students)
+            {
+                if (student.HomeTown == cityName)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+            }
+
+        }
+    }
+
+    class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string HomeTown { get; set; }
+    }
+}
