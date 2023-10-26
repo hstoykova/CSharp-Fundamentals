6713aa5e1af535756808ed6a1260e03ff97272ad
hstoykova <hristina.pencheva@gmail.com>
2023-10-26 21:15:50 +0300

    "03. Songs" solved

---
+++ F-Lab-ObjectsAndClasses/03.Songs/Program.cs
@@ -0,0 +1,79 @@
+﻿/*
+3
+favourite_DownTown_3:14
+favourite_Kiss_4:16
+favourite_Smooth Criminal_4:01
+favourite
+
+4
+favourite_DownTown_3:14
+listenLater_Andalouse_3:24
+favourite_In To The Night_3:58
+favourite_Live It Up_3:48
+listenLater
+
+2
+like_Replay_3:15
+ban_Photoshop_3:48
+all
+
+ */
+
+namespace _03.Songs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSongs = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>(); // List from class Song
+
+            for (int i = 1; i <= numberOfSongs; i++)
+            {
+                string[] data = Console.ReadLine().Split("_");
+
+                string type = data[0];
+                string name = data[1];
+                string time = data[2];
+
+                Song song = new Song(); // object Song
+
+                song.TypeList = type;
+                song.Name = name;
+                song.Time = time;
+
+                songs.Add(song);
+            }
+
+            string typeList = Console.ReadLine();
+
+            if (typeList == "all")
+            {
+                foreach (Song song in songs) // for every object "Song" in List songs
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (Song song in songs)
+                {
+                    if (song.TypeList == typeList)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+            
+        }
+    }
+
+    class Song //create a class named "Song"
+    {
+        public string TypeList { get; set; } //property
+
+        public string Name { get; set; } //property
+
+        public string Time { get; set; } //property
+    }
+}
