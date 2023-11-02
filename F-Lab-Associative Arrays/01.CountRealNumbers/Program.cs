d61c2f0d0400562bd065e50845a9a0e102d412d3
hstoykova <hristina.pencheva@gmail.com>
2023-11-02 22:40:02 +0200

    "01. Count Real Numbers" solved

---
+++ F-Lab-Associative Arrays/01.CountRealNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+             
+            SortedDictionary<int, int> counts = new SortedDictionary<int, int>();
+
+            foreach (int number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts.Add(number, 1);
+                }
+            }
+
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
