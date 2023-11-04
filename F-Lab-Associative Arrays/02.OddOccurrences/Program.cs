9726d5dbc4778957a0ce4ce9e425f64a344bdea8
hstoykova <hristina.pencheva@gmail.com>
2023-11-04 12:51:05 +0200

    "02. Odd Occurrences" solved

--- F-Lab-Associative Arrays/02.OddOccurrences/Program.cs
+++ F-Lab-Associative Arrays/02.OddOccurrences/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace _02.OddOccurrences
+﻿/*
+Java C# PHP PHP JAVA C java
+
+3 5 5 hi pi HO Hi 5 ho 3 hi pi
+ */
+
+namespace _02.OddOccurrences
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] words = Console.ReadLine()
+                .Split();
+            
+            Dictionary<string, int> count = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string wordToLower = word.ToLower();
+
+                if (count.ContainsKey(wordToLower))
+                {
+                    count[wordToLower]++;
+                }
+                else
+                {
+                    count.Add(wordToLower, 1);
+                }
+            }
+
+            foreach (var occur in count)
+            {
+                if (occur.Value % 2 != 0)
+                {
+                    Console.Write(occur.Key + " ");
+                }
+            }
         }
     }
 }
