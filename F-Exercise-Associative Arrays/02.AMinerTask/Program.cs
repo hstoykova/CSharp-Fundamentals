bb1052b3519e3aa40d70064ac2331bf0e4a2c95c
hstoykova <hristina.pencheva@gmail.com>
2023-11-04 18:06:42 +0200

    "02. A Miner Task" solved

--- F-Exercise-Associative Arrays/02.AMinerTask/Program.cs
+++ F-Exercise-Associative Arrays/02.AMinerTask/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace _02.AMinerTask
+﻿/*
+Gold
+155
+Silver
+10
+Copper
+17
+stop
+
+ */
+
+namespace _02.AMinerTask
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, uint> resources = new Dictionary<string, uint>();
+
+            string resource;
+            uint quantity;
+
+            while ((resource = Console.ReadLine()) != "stop")
+            {
+                if (!resources.ContainsKey(resource))
+                {
+                    resources.Add(resource, 0);
+                }
+
+                quantity = uint.Parse(Console.ReadLine());
+                resources[resource] += quantity;
+
+            }
+
+            foreach (KeyValuePair<string, uint> resourcesPair in resources)
+            {
+                Console.WriteLine($"{resourcesPair.Key} -> {resourcesPair.Value}");
+            }
         }
     }
 }
