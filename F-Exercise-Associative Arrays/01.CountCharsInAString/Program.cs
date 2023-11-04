4ca2aa538f6c32df5ec18ad670660f299d4db79a
hstoykova <hristina.pencheva@gmail.com>
2023-11-04 17:42:02 +0200

    "01. Count Chars in a String" solved

---
+++ F-Exercise-Associative Arrays/01.CountCharsInAString/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _01.CountCharsInAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> occurrences = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentLetter = input[i];
+
+                if (currentLetter == ' ')
+                {
+                    continue;
+                }
+
+                if (!occurrences.ContainsKey(currentLetter))
+                {
+                    occurrences.Add(currentLetter, 0);
+                }
+
+                occurrences[currentLetter]++;
+            }
+
+            foreach (KeyValuePair<char, int> pair in occurrences)
+            {
+                char charcter = pair.Key;
+                int count = pair.Value;
+                Console.WriteLine($"{charcter} -> {count}");
+            }
+        }
+    }
+}
