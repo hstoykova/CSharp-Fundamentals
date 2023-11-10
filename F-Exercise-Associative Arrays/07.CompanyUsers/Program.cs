8691065622881c0b4597d05f694d5b6768e07381
hstoykova <hristina.pencheva@gmail.com>
2023-11-10 16:04:55 +0200

    "07. Company Users" solved

---
+++ F-Exercise-Associative Arrays/07.CompanyUsers/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _07.CompanyUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, List<string>> companyUsers = new Dictionary<string, List<string>>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] arguments = input.Split(" -> ");
+                string companyName = arguments[0];
+                string employeeId = arguments[1];
+
+                if (!companyUsers.ContainsKey(companyName))
+                {
+                    companyUsers.Add(companyName, new List<string> {employeeId});
+                }
+                else
+                {
+                    var ids = companyUsers.GetValueOrDefault(companyName);
+
+                    if (!ids.Contains(employeeId))
+                    {
+                        ids.Add(employeeId);
+                    }
+                }
+
+            }
+            //KeyValuePair<string, Student> studentPair in filteredStudents
+            foreach (KeyValuePair<string, List<string>> pair in companyUsers)
+            {
+                Console.WriteLine(pair.Key);
+
+                foreach (var id in pair.Value)
+                {
+                    Console.WriteLine($"-- {id}");
+                }
+            }
+        }
+    }
+}
