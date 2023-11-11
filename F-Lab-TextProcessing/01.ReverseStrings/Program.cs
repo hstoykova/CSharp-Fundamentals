a5b221d9cf72d8a83039644f7bc08954269d0f8c
hstoykova <hristina.pencheva@gmail.com>
2023-11-11 14:41:29 +0200

    "01. Reverse Strings" solved

---
+++ F-Lab-TextProcessing/01.ReverseStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+helLo
+Softuni
+bottle
+end
+
+ */
+
+namespace _01.ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                char[] stringArray = input.ToCharArray();
+                Array.Reverse(stringArray);
+                string reversedStr = new string(stringArray);
+                Console.WriteLine($"{input} = {reversedStr}");
+            }
+        }
+    }
+}
