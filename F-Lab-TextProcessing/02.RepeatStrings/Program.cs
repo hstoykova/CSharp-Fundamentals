22365fd10d559c683e7e09dbcc34022334e41559
hstoykova <hristina.pencheva@gmail.com>
2023-11-11 15:04:11 +0200

    "02. Repeat Strings" solved

---
+++ F-Lab-TextProcessing/02.RepeatStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+hi abc add
+ */
+
+using System.Text;
+
+namespace _02.RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ");
+            StringBuilder result = new StringBuilder();
+
+            foreach (string word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    result.Append(word);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
