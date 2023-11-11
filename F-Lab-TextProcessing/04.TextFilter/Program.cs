c0cbf81d96659ef386a2ce95577f3bc28a56fffc
hstoykova <hristina.pencheva@gmail.com>
2023-11-11 15:35:35 +0200

    "04. Text Filter" solved

--- F-Lab-TextProcessing/04.TextFilter/Program.cs
+++ F-Lab-TextProcessing/04.TextFilter/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace _04.TextFilter
+﻿/*
+Linux, Windows
+It is not Linux, it is GNU/Linux. Linux is merely the kernel, while GNU adds the functionality. Therefore we owe it to them by calling the OS GNU/Linux! Sincerely, a Windows client
+
+ */
+using System.Linq;
+
+namespace _04.TextFilter
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] bannedWords = Console.ReadLine().Split(", ").ToArray();
+            string text = Console.ReadLine();
+
+            foreach (string word in bannedWords)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+
+            Console.WriteLine(text);
         }
     }
 }
