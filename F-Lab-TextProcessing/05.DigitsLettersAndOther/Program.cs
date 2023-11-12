ecadf0ab0f6ad5376cec4c5b7bde79beb44e2612
hstoykova <hristina.pencheva@gmail.com>
2023-11-12 16:19:25 +0200

    "05. Digits, Letters and Other" solved

--- F-Lab-TextProcessing/05.DigitsLettersAndOther/Program.cs
+++ F-Lab-TextProcessing/05.DigitsLettersAndOther/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace _05.DigitsLettersAndOther
+﻿/*
+Agd#53Dfg^&4F53
+ */
+
+namespace _05.DigitsLettersAndOther
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(new string (input.Where(x=>char.IsDigit(x)).ToArray()));
+            Console.WriteLine(new string (input.Where(x=>char.IsLetter(x)).ToArray()));
+            Console.WriteLine(new string (input.Where(x=>!char.IsLetter(x) && !char.IsDigit(x)).ToArray()));
         }
     }
 }
