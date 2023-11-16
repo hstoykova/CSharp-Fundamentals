01639a6df579ba619d9ac99cc9b63bc2af7e3e66
hstoykova <hristina.pencheva@gmail.com>
2023-11-16 15:30:02 +0200

    "01. Valid Usernames" solved

---
+++ F-Exercise-Text Processing/01.ValidUsernames/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+sh, too_long_username, !lleg@l ch@rs, jeffbutt
+ */
+
+namespace _01.ValidUsernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usenamames = Console.ReadLine().Split(", ");
+
+            foreach (string name in usenamames)
+            {
+                if (name.Length >= 3 && name.Length <= 16 && isValidUsername(name))
+                {
+                    
+                        Console.WriteLine(name);
+                    
+                }
+            }
+        }
+
+        private static bool isValidUsername(string name)
+        {
+            return name.All(ch => char.IsLetterOrDigit(ch) || ch == '-' || ch == '_');
+        }
+    }
+}
