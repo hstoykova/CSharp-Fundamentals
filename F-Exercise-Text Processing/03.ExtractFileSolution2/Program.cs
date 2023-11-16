e999b3bcbe36e4eafb31df836b7527506d4502f5
hstoykova <hristina.pencheva@gmail.com>
2023-11-16 21:08:31 +0200

    "04. Caesar Cipher" solved

---
+++ F-Exercise-Text Processing/03.ExtractFileSolution2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _03.ExtractFileSolution2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TOVA NE RABOTI V JUDGE, NO PO PRINCIP RABOTI
+            string filePath = Console.ReadLine();
+
+            Console.WriteLine($"File name: {Path.GetFileNameWithoutExtension(filePath)}");
+            Console.WriteLine($"File extension: {Path.GetExtension(filePath).Replace(".", "")}");
+        }
+    }
+}
