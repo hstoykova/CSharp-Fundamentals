3d50843731e6ff8b0ae291ca18a90075120d45d5
hstoykova <hristina.pencheva@gmail.com>
2023-11-16 21:20:47 +0200

    "04. Caesar Cipher" - solution 2

---
+++ F-Exercise-Text Processing/04.CaesarCipherSolution2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace _04.CaesarCipherSolution2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().Select(c => (char)(c + 3)).ToArray();
+
+            Console.WriteLine(string.Join("", input));
+        }
+    }
+}
