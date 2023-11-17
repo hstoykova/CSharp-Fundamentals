7bc15e32c1f8a9597dea2bdfa8e27025bd30eb9d
hstoykova <hristina.pencheva@gmail.com>
2023-11-17 12:14:09 +0200

    "01. Match Full Name" solved

---
+++ F-Lab-RegularExpressions/01.MatchFullName/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Bethany Taylor, Oliver miller, sophia Johnson, SARah Wilson, John Smith, Sam       Smith
+ */
+
+using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+            MatchCollection matchedNames = Regex.Matches(input, regex);
+
+            foreach (Match name in matchedNames) 
+            {
+                Console.Write(name.Value + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
