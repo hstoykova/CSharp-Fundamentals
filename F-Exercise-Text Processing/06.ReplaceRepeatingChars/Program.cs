a438ef49dbff10b42193724a0ad5a4f5ccef91ba
hstoykova <hristina.pencheva@gmail.com>
2023-11-17 15:50:14 +0200

    "06. Replace Repeating Chars" solved

---
+++ F-Exercise-Text Processing/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace _06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(input[0]);
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] != input[i - 1])
+                {
+                    sb.Append(input[i]);
+                }
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
