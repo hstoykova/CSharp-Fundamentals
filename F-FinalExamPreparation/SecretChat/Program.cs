9a74d14944fec3f050688c6394f476103c3b9222
hstoykova <hristina.pencheva@gmail.com>
2023-11-23 13:26:45 +0200

    "01. Secret Chat" solved

--- F-FinalExamPreparation/SecretChat/Program.cs
+++ F-FinalExamPreparation/SecretChat/Program.cs
@@ -1,10 +1,70 @@
-﻿namespace SecretChat
+﻿/*
+heVVodar!gniV
+ChangeAll:|:V:|:l
+Reverse:|:!gnil
+InsertSpace:|:5
+Reveal
+
+Hiware?uiy
+ChangeAll:|:i:|:o
+Reverse:|:?uoy
+Reverse:|:jd
+InsertSpace:|:3
+InsertSpace:|:7
+Reveal
+
+*/
+
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace SecretChat
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            string input = Console.ReadLine();
+
+            string instuctions;
+            while ((instuctions = Console.ReadLine()) != "Reveal")
+            {
+                string[] commands = instuctions.Split(":|:");
+
+                switch (commands[0])
+                {
+                    case "InsertSpace":
+                        int index = int.Parse(commands[1]);
+                        input = input.Insert(index, " ");
+                        break;
+
+                    case "Reverse":
+                        string substringStr = commands[1];
+                        int substringIndex = input.IndexOf(substringStr);
+
+                        if (substringIndex == -1)
+                        {
+                            Console.WriteLine("error");
+                            continue;
+                        }
+                        input = input.Remove(substringIndex, substringStr.Length);
+                        string reversedSubstr = new string(substringStr.Reverse().ToArray()); // ToString() can be ToArray()
+                        input += reversedSubstr;
+                        break;
+
+                    case "ChangeAll":
+                        string substring = commands[1]; 
+                        string replacement = commands[2];
+                        input = input.Replace(substring, replacement);
+                        break;
+                }
+
+                Console.WriteLine(input);
+            }
+
+            Console.WriteLine($"You have a new text message: {input}");
         }
     }
 }
---
+++ F-FinalExamPreparation/test/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Educative";
+            char[] stringArray = str.ToCharArray();
+            Array.Reverse(stringArray);
+            string reversedStr = new string(stringArray);
+            Console.Write($"Actual String is : {str} \n");
+            Console.Write($"Reversed String is : {reversedStr} ");
+        }
+    }
+}
